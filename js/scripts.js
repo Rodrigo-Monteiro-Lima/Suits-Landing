b05ecf33f9317156456ac77a00fd79de7ba5a571
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-22 18:14:38 -0300

    fix: link

--- js/scripts.js
+++ js/scripts.js
@@ -8,20 +8,23 @@ const slides = document.querySelectorAll('.banner');
 const dots = document.querySelectorAll('.dot');
 let slideIndex = 0;
 
-const smoothScroll = (e) => {
+function smoothScroll(e) {
   e.preventDefault();
+
   const href = this.getAttribute('href');
   const offsetTop = document.querySelector(href).offsetTop;
+
   scroll({
     top: offsetTop,
     behavior: 'smooth',
   });
+
   setTimeout(() => {
     if (menu.classList.contains('menu-active')) {
       menu.classList.remove('menu-active');
     }
   }, 500);
-};
+}
 
 const showSlides = () => {
   slides.forEach((slide, index) => {
